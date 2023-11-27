f867b0f111e124286d31083c72a9155136e9603d
dachadrya <daya.acharya@gmail.com>
2023-11-26 20:47:56 -0500

    added FunctionBasic class

---
+++ src/FunctionBasic.java
@@ -0,0 +1,27 @@
+public class FunctionBasic {
+
+    public static void main(String[] args) {
+       FunctionBasic fb = new FunctionBasic();
+        int a = 4;
+        int b = 8;
+        fb.add(a,b);
+        fb.subtract(a,b);
+        fb.multiply(a,b);
+
+    }
+public void add(int a, int b){
+        int result = a+b;
+        System.out.println(result);
+}
+public int subtract(int a, int b){
+        int result=a-b;
+        System.out.println(result);
+        return result;
+}
+public void multiply(int a, int b){
+        int result;
+        result = a*b;
+        System.out.println(result);
+
+    }
+}
