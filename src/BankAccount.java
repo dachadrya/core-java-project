f7fb668b83d51d5372c065c1a733b808bab083a4
dachadrya <daya.acharya@gmail.com>
2023-11-27 17:46:49 -0500

    added BankAccount class

---
+++ src/BankAccount.java
@@ -0,0 +1,22 @@
+import com.sun.jdi.PathSearchingVirtualMachine;
+public class BankAccount{
+    String accountHolderName;
+    String accountNumber;
+    double accountBalance;
+    String routingNumber;
+    String socialSecurityNumber;
+    String accountHolderAddress;
+    String accountHolderDOB;
+    String accountHolderGender;
+    String  accountHolderPhoneNumber;
+    String accountHolderEmail;
+    Boolean isMinorAccount;
+    final String BANK_NAME="TRUIST";
+
+    public static void main(String[] args) {
+        BankAccount dayaAccount = new BankAccount();
+        BankAccount abcAccount = new BankAccount();
+
+    }
+
+}
