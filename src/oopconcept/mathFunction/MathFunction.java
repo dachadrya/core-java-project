c2db4e17726d46ebdd462745a475e410726bf1f2
dachadrya <daya.acharya@gmail.com>
2023-12-03 13:41:41 -0500

    Practice some of Math class  given function : abs, round, ceil, floor, max, min, signum

---
+++ src/oopconcept/mathFunction/MathFunction.java
@@ -0,0 +1,16 @@
+package oopconcept.mathFunction;
+
+public class MathFunction {
+
+    public static void main(String[] args) {
+
+
+     System.out.println("Maximum of numbers 1.1 ,2.2: "+Math.max(1.1, 2.2));
+     System.out.println("Minimum of numbers 1, 2: "+Math.min(1, 2));
+      System.out.println("Absolute of number -12: "+Math.abs(-12));
+        System.out.println("Round of number -12.7: "+Math.round(-12.7));
+      System.out.println("Ceiling of number 3.3: "+Math.ceil(3.3));
+        System.out.println("Floor of number 5.9: "+Math.floor(5.9));
+      System.out.println("Signum of number -12: "+Math.signum(-12));
+    }
+}
